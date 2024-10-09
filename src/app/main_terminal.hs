7274f502918a254db73f41c11d30247d78bb3d21
AlexisNovo <alexis8.pl@outlook.com>
2024-10-09 07:30:19 -0600

    v0.0.1 minor updates

--- src/app/main_terminal.hs
+++ src/app/main_terminal.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import System.Console.Haskeline
+    ( defaultSettings, getInputChar, outputStrLn, runInputT, InputT )
+import System.Console.ANSI ( clearScreen, setCursorPosition )
+import Control.Monad.IO.Class (liftIO)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+    where 
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputChar "Press a key (q to quit): "
+      case minput of
+        Just 'q' -> outputStrLn "Exiting..."
+        Just c   -> do
+          liftIO clearScreen
+          liftIO $ setCursorPosition 0 0
+          outputStrLn ("Key pressed: " ++ [c])
+          loop
+        Nothing  -> loop
+        
