10457814d804fa40732a8db7231174317ec87ccf
AlexisNovo <alexis8.pl@outlook.com>
2024-10-11 07:19:55 -0600

    minor fixes

--- src/app/main_terminal.hs
+++ src/app/main_terminal.hs
@@ -23,12 +23,15 @@ import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
 
+
+{-- Data class--}
 data ScreenClass = ScreenClass
   {text:: String
   , images :: String
   }
   deriving (Show, Generic)
 
+{--JSON data--}
 data Info = Info
   { screen_prin :: ScreenClass
   , screen_1 :: ScreenClass
