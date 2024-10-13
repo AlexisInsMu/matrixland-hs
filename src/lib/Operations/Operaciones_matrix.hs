fc18b09f444f6a708aa828e4a2db3c71308a99f6
LauraDorantes <ldrnts@gmail.com>
2024-10-13 02:23:42 -0600

    minor fixes

--- src/lib/Operations/Operaciones_matrix.hs
+++ src/lib/Operations/Operaciones_matrix.hs
@@ -1,6 +1,7 @@
 module Operations.Operaciones_matrix
     ( prodEscalar
     , multiMatriz
+    , identMatriz
     ) where
 
 import Data.Array
@@ -19,6 +20,20 @@ multiMatriz p q =
         m = noFilas p
         n = noColumnas q
 
+-- Matriz Identidad:
+-- n guarda el tamaño de la matriz, ya que para la identidad es nxn
+-- El primer <map> itera sobre cada fila i. Para c/valor, genera un fila con una lambda.
+-- El segundo <map>, para cada fila respectivamente itera sobre cada columna j
+-- y la lambda define si en es posición va un 1 o 0. Esto lo hace con el if.
+-- Si los indice i y j son iguales (osea estan en la diagonal principal)
+-- pone un cero, si son diferentes pone un cero.
+-- Y así de vuelve una lista de n listas con n valores.
+
+--map: Le aplica la función de los parentesis a la lista que ponemos, elemento a elemento.
+
+identMatriz :: Int -> [[Int]]
+identMatriz n = map(\i -> map (\j -> if i == j then 1 else 0) [0..n-1]) [0..n-1]
+
 -- Funciones auxiliares para obtener filas y columnas
 fila :: Int -> Array (Int, Int) a -> Array Int a
 fila i p = ixmap (1, n) (\j -> (i, j)) p
@@ -36,17 +51,5 @@ noColumnas :: Array (Int, Int) a -> Int
 noColumnas q = n
     where (_, (_, n)) = bounds q
 
--- Matriz Identidad
--- n guarda el tamaño de la matriz, ya que para la identidad es nxn
---El primer <map> itera sobre cada fila i. Para c/valor, genera un fila con una lambda.
---El segundo <map>, para cada fila respectivamente itera sobre cada columna j
--- y la lambda define si en es posición va un 1 o 0. Esto lo hace con el if.
--- Si los indice i y j son iguales (osea estan en la diagonal principal)
--- pone un cero, si son diferentes pone un cero.
---Y así de vuelve una lista de n listas con n valores.
 
---Map: Le aplica la función de los parentesis a la lista que ponemos, elemento a elemento.
-
-identMatriz :: Int -> [[Int]]
-identMatriz n = map(\i -> map (\j -> if i == j then 1 else 0) [0..n-1]) [0..n-1]
 
