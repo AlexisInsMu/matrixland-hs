a71ccabb045f155567c5b3261ead9c4fb4f03636
AlexisNovo <alexis8.pl@outlook.com>
2024-10-15 07:15:17 -0600

    v0.2.1 minor fixes

--- src/app/main_terminal.hs
+++ src/app/main_terminal.hs
@@ -11,7 +11,7 @@ import Control.Monad.IO.Class (liftIO)
 
 import System.Process (system)
 
--- main :: IO ()
+-- mainterminal :: IO ()
 -- main = runInputT defaultSettings loop
 --     where
 --     loop :: InputT IO ()
@@ -39,9 +39,14 @@ data ScreenClass = ScreenClass
 
 {--JSON data--}
 data Info = Info
-  { screen_prin :: ScreenClass
+  { screen_select :: ScreenClass
   , screen_1 :: ScreenClass
   , sreeen_menu :: ScreenClass
+  , screen_2 :: ScreenClass
+  , screen_3 :: ScreenClass
+  , screen_quiz1  :: ScreenClass
+  , screen_quiz2  :: ScreenClass
+  , screen_quiz3  :: ScreenClass
   } deriving (Show, Generic)
 
 instance FromJSON ScreenClass
@@ -49,6 +54,7 @@ instance ToJSON ScreenClass
 
 instance FromJSON Info
 instance ToJSON Info
+
 {-- DisplayScreen, imprime el contenido de un json interactuable con numeros--}
 displayScreen :: FilePath -> (String -> IO())  -> (Info -> ScreenClass) -> String -> IO  ()
 displayScreen filePath handleInput selectScreen control do
@@ -70,30 +76,27 @@ displayScreen filePath handleInput selectScreen control do
         let screen = selectScreen info
         putStrLn $ text screen
         hFlush stdout
-        handleInput control
         waitForAnyKey
+        handleInput control
       Nothing -> "Failed to parse Json"
-
-
-main :: IO ()
-main = runInputT defaultSettings loop
-    where 
-    loop :: InputT IO ()
-    loop = do
-      minput <- getInputChar "Press a key (q to quit): "
-      case minput of
-        Just 'q' -> outputStrLn "Exiting..."
-        Just c   -> do
-          liftIO clearScreen
-          liftIO $ setCursorPosition 0 0
-          outputStrLn ("Key pressed: " ++ [c])
-          loop
-        Nothing  -> loop
         
-
-main :: IO ()
-main = do
-  displayScreen "../data/info.json" ejecutarOp sreeen_menu ""
+ejecutarOp_a :: String -> IO ()
+ejecutarOp_a op = case op of
+  "1" -> do
+    mainterminal
+  "2" -> do
+    putStrLn "Aqui va algo"
+  "3" -> do 
+    clearScreen
+    putStrLn "Saliendo..."
+    liftIO exitSuccess
+  "4" -> do
+    clearScreen
+    putStrLn "Saliendo..."
+  _ -> do
+    clearScreen
+    putStrLn "No válida. Intenta de nuevo."
+    main
 
 ejecutarOp :: String -> IO ()
 ejecutarOp op = case op of
@@ -106,63 +109,27 @@ ejecutarOp op = case op of
   "4" -> do
     clearScreen
     putStrLn "Saliendo..."
+    liftIO exitSuccess
   _ -> do
     clearScreen
     putStrLn "No válida. Intenta de nuevo."
-    main
+    mainterminal
 
 ejecutarOp1 :: String -> IO ()
 ejecutarOp1 op1 = case op1 of
-    "1" -> do
-        displayScreen "../data/info.json" ejecutarOp screen_info_1 "1"
-        putStrLn ""
-        putStrLn "-------INFORMACIÓN-------"
-        putStrLn "Antes que nada, debemos recordar que la potencia de una matriz no "
-        "siempre se puede calcular. Sólo es posible cuando "
-        "la matriz es cuadrada, es decir, cuando tiene el mismo número de filas que de columnas."
-        putStrLn "La peculiaridad de la potenciación de las matrices es que, en muchas matrices, 
-        las potencias siguen un patrón. Por ejemplo, las potencia n-ésima de una matriz diagonal 
-        A es también una matriz diagonal cuyos elementos de la diagonal son las potencias ""
-        n-ésimas de los elementos de la diagonal de la matriz A:"
-        ejecutarOp "1"
-    "2" -> do
-        displayScreen "../data/info.json" ejecutarOp screen_info_2 "1"
-        putStrLn ""
-        putStrLn "-------QUIZ POTENCIA DE MATRICES-------"
-        putStrLn ""
-        ejecutarOp "1"
-    "3" -> do
-        displayScreen "../data/info.json" ejecutarOp screen_info_3 "1"
-        putStrLn ""
-        putStrLn "-------CALCULADORA PARA POTENCIAS DE MATRICES-------"
-        putStrLn ""
-        ejecutarOp "1"
-    "4" -> do
-        putStrLn ""
-        main
-    _   -> do
-        putStrLn ""
-        putStrLn "No válida. Intenta de nuevo."
-        putStrLn ""
-        ejecutarOp "1"
   "1" -> do
-    putStrLn ""
-    putStrLn "-------INFORMACIÓN-------"
-    putStrLn ""
-    ejecutarOp "1"
+    displayScreen "../data/info.json" ejecutarOp screen_1 "1"
   "2" -> do
-    putStrLn ""
-    putStrLn "-------QUIZ POTENCIA DE MATRICES-------"
-    putStrLn ""
-    ejecutarOp "1"
+    displayScreen "../data/info.json" ejecutarOp screen_quiz1 ""
+
   "3" -> do
     putStrLn ""
     putStrLn "-------CALCULADORA PARA POTENCIAS DE MATRICES-------"
     putStrLn ""
-    ejecutarOp "1"
+    displayScreen "../data/info.json" ejecutarOp screen_1 ""
   "4" -> do
     putStrLn ""
-    main
+    mainterminal
   _ -> do
     putStrLn ""
     putStrLn "No válida. Intenta de nuevo."
@@ -172,23 +139,17 @@ ejecutarOp1 op1 = case op1 of
 ejecutarOp2 :: String -> IO ()
 ejecutarOp2 op2 = case op2 of
   "1" -> do
-    putStrLn ""
-    putStrLn "-------INFORMACIÓN-------"
-    putStrLn ""
-    ejecutarOp "2"
+    displayScreen "../data/info.json" ejecutarOp screen_2 "2"
   "2" -> do
-    putStrLn ""
-    putStrLn "-------QUIZ MATRIZ IDENTIDAD-------"
-    putStrLn ""
-    ejecutarOp "2"
+    displayScreen "../data/info.json" ejecutarOp screen_quiz2 ""
   "3" -> do
     putStrLn ""
     putStrLn "-------CALCULADORA PARA MATRICES IDENTIDAD-------"
     putStrLn ""
-    ejecutarOp "2"
+    displayScreen "../data/info.json" ejecutarOp screen_2 ""
   "4" -> do
     putStrLn ""
-    main
+    mainterminal
   _ -> do
     putStrLn ""
     putStrLn "No válida. Intenta de nuevo."
@@ -198,26 +159,35 @@ ejecutarOp2 op2 = case op2 of
 ejecutarOp3 :: String -> IO ()
 ejecutarOp3 op3 = case op3 of
   "1" -> do
-    putStrLn ""
-    putStrLn "-------INFORMACIÓN-------"
-    putStrLn ""
-    ejecutarOp "3"
+    displayScreen "../data/info.json" ejecutarOp screen_3 "3"
   "2" -> do
     putStrLn ""
     putStrLn "-------QUIZ MULTIPLICACIÓN DE MATRICES-------"
     putStrLn ""
-    ejecutarOp "3"
+    displayScreen "../data/info.json" ejecutarOp screen_quiz3 ""
   "3" -> do
     putStrLn ""
     putStrLn "-------CALCULADORA PARA MULTIPLICACIÓN DE MATRICES-------"
     putStrLn ""
-    ejecutarOp "3"
+    displayScreen "../data/info.json" ejecutarOp screen_3 ""
   "4" -> do
     putStrLn ""
-    main
+    mainterminal
   _ -> do
     putStrLn ""
     putStrLn "No válida. Intenta de nuevo."
     putStrLn ""
     ejecutarOp "3"
+
+mainterminal :: IO ()
+mainterminal = do
+  displayScreen "../data/info.json" ejecutarOp sreeen_menu ""
+
+main :: IO ()
+main = do
+  displayScreen "../data/info.json" ejecutarOp_a sreeen_select ""
+
+
+
+
 
