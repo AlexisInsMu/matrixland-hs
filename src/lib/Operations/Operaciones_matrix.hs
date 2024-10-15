685daa32fd40de728f52309131f0e511b106e19c
AlexisNovo <alexis8.pl@outlook.com>
2024-10-14 20:30:00 -0600

    minor fixes

--- src/lib/Operations/Operaciones_matrix.hs
+++ src/lib/Operations/Operaciones_matrix.hs
@@ -3,7 +3,10 @@ module Operations.Operaciones_matrix
     , multiMatriz
     ) where
 
-import Data.Array
+import Data.Array ( array, bounds, elems, ixmap, Array )
+
+identity :: Num a => Int -> Array (Int, Int) a
+identity n = array ((1, 1), (n, n)) [((i, j), fromIntegral (fromEnum (i == j))) | i <- [1..n], j <- [1..n]]
 
 -- Definimos una función para obtener el producto escalar de los vectores.
 prodEscalar :: Num a => Array Int a -> Array Int a -> a
@@ -35,3 +38,34 @@ noFilas p = m
 noColumnas :: Array (Int, Int) a -> Int
 noColumnas q = n
     where (_, (_, n)) = bounds q
+
+
+{-- Potenciación de matrices --}
+{--
+
+--}
+
+{-- Fast EXPO
+    Esta función se encarga de realizar la potenciación de matrices de forma rápida.
+    Recibe una matriz y un número entero, y devuelve la matriz elevada a la potencia n.
+    La función se basa en la propiedad de que si n es par, entonces A^n = (A^2)^(n/2), y si n es impar, entonces A^n = A * A^(n-1).
+    De esta forma, se reduce el número de multiplicaciones necesarias para obtener la matriz elevada a la potencia n.
+ --}
+fastExpo :: Num a => Array (Int, Int) a -> Int -> Array (Int, Int) a
+fastExpo p 1 = p
+fastExpo p n
+    | even n = fastExpo (multiMatriz p p) (n `div` 2)
+    | otherwise = multiMatriz p (fastExpo p (n - 1))
+
+-- Función para obetner el inverso de una matriz
+inverso :: Num a => Array (Int, Int) a -> Array (Int, Int) a
+inverso p = array ((1, 1), (m, n)) [((i, j), -p!(i, j)) | i <- [1..m], j <- [1..n]]
+    where
+        m = noFilas p
+        n = noColumnas p
+
+exponention :: Num a => Array (Int, Int) a -> Int -> Array (Int, Int) a
+exponention p n
+    | n == 0 = identity (noFilas p)
+    | n < 0 = fastExpo (inverso p) -n
+    | otherwise = fastExpo p n
